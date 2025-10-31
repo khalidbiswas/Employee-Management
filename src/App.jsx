f1d202faffb36b578ff5816dc88321cb63335063
Khalid Bin Hasan <khalidbinhasan.kbh@gmail.com>
2025-10-31 11:01:09 +0600

    project sat up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './assets/components/Auth/Login';
+
+const App = () => {
+  return (
+    <>
+      <Login></Login>
+    </>
+  );
+};
+
+export default App;
