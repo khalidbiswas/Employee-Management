5263aad4811dd85c47de9fc6bc44b8f1e6cdab8b
Khalid Bin Hasan <khalidbinhasan.kbh@gmail.com>
2025-10-31 12:29:43 +0600

    login page ui done

--- src/assets/components/Auth/Login.jsx
+++ src/assets/components/Auth/Login.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+const handleSubmit = (e) => {
+    e.preventDefault()
+console.log('Hello')
+}
 
 const Login = () => {
     return (
-        <div>
+        <div className="flex items-center justify-center min-h-screen bg-yellow-300 ">
+            <div className="w-[380px]  shadow-2xl rounded-2xl bg-blue-300 p-8 border border-gray-200 hover:shadow-teal-400/40 transition-all duration-300">
+                <h2 className="text-center text-3xl font-bold text-gray-800 mb-8">
+                    Welcome Back 👋
+                </h2>
 
+                <form onSubmit={(e)=>{handleSubmit(e)}} className="flex flex-col space-y-5">
+
+                    <input
+                        type="email"
+                        required
+                        placeholder="Enter your email"
+                        className="border border-gray-300 bg-white text-gray-800 placeholder:text-gray-400 rounded-full px-5 py-3 text-base focus:outline-none focus:ring-2 focus:ring-teal-500 shadow-sm transition"
+                    />
+
+                    <input
+                        type="password"
+                        required
+                        placeholder="Enter your password"
+                        className="border border-gray-300 bg-white text-gray-800 placeholder:text-gray-400 rounded-full px-5 py-3 text-base focus:outline-none focus:ring-2 focus:ring-teal-500 shadow-sm transition"
+                    />
+
+                    <button
+                        type="submit"
+                        className="mt-2 bg-gradient-to-r from-teal-500 to-emerald-500 hover:from-emerald-500 hover:to-teal-500 text-white font-semibold py-3 rounded-full text-lg transition-all duration-300 shadow-lg hover:shadow-emerald-400/40"
+                    >
+                        Log in
+                    </button>
+                </form>
+
+                <p className="text-center text-sm text-gray-700 mt-6">
+                    Don’t have an account?{" "}
+                    <a href="#" className="text-teal-600 font-medium hover:underline">
+                        Sign up
+                    </a>
+                </p>
+            </div>
         </div>
+
     );
 };
 
